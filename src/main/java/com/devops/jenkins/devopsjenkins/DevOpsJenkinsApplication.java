16ff9441e4e7dff5fd68fbe8119664e7db841916
okaya <okan93kaya@gmail.com>
2019-01-13 16:46:34 +0300

    edit commit

--- src/main/java/com/devops/jenkins/devopsjenkins/DevOpsJenkinsApplication.java
+++ src/main/java/com/devops/jenkins/devopsjenkins/DevOpsJenkinsApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevOpsJenkinsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("test");
 		SpringApplication.run(DevOpsJenkinsApplication.class, args);
 	}
 
