6f0220abf8a8c85c12a784710a1d3f0d15a35fe4
okaya <okan93kaya@gmail.com>
2019-01-13 16:38:27 +0300

    edit commit

---
+++ src/main/java/com/devops/jenkins/devopsjenkins/DevOpsJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.devops.jenkins.devopsjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsJenkinsApplication.class, args);
+	}
+
+}
+
